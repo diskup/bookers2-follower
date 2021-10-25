baafa306cb4f5866ed3f7c60e3365608dc6a0422
“diskup” <taigaa.1998@gmail.com>
2021-10-25 16:18:16 +0000

    3回目

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,8 +12,10 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to book_path(@book), notice: "You have created book successfully."
+      redirect_to book_path(@book)
+      flash[:notice] = "You have created book successfully."
     else
+      @user = current_user
       @books = Book.all
       render 'index'
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @books = @user.books.page(params[:page])
+    @books = Book.includes(:@user)
     @book = Book.new
   end
 
@@ -18,9 +18,9 @@ class UsersController < ApplicationController
 
   def update
     if @user.update(user_params)
-      redirect_to users_path(@user), notice: "You have updated user successfully."
+      redirect_to user_path(@user.id), notice: "You have updated user successfully."
     else
-      render "show"
+      render :edit
     end
   end
 
